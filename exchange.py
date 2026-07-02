f859a743dce889c735fa93ea839270a40b1ffbec
expert-gyasho <gyashoexpert@gmail.com>
2026-07-02 09:04:57 +0530

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -1,1 +1,34 @@
+import ccxt
+import pandas as pd
 
+exchange = ccxt.binance({
+    "enableRateLimit": True,
+})
+
+
+def get_ohlcv(symbol, timeframe="4h", limit=200):
+    """
+    Download OHLCV data from Binance
+    """
+
+    candles = exchange.fetch_ohlcv(
+        symbol,
+        timeframe=timeframe,
+        limit=limit
+    )
+
+    df = pd.DataFrame(
+        candles,
+        columns=[
+            "timestamp",
+            "open",
+            "high",
+            "low",
+            "close",
+            "volume",
+        ],
+    )
+
+    df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+
+    return df
